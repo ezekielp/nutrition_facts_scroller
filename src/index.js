0a85efa97e7303403d10bdc0dc555bd8b5cfc490
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-01-06 14:46:49 -0500

    Get basic IntersectionObserver function working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import './styles/index.scss';
 
+let newBox;
 
+window.addEventListener("load", (e) => {
+    newBox = document.querySelector("#new-box");
 
+    createObserver();
+}, false);
+
+const createObserver = () => {
+    let observer;
+    
+    let options = {
+      root: null,
+      rootMargin: "0px 0px 0px 0px",
+      threshold: 0.25
+    };
+
+    observer = new IntersectionObserver(handleScrollOnto, options);
+    observer.observe(newBox);
+}
+
+const handleScrollOnto = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.style.visibility = "visible";
+        }
+    })
+}
 
 
