c7bcf250a0bdc1f110b06b35d8196e421c8af64d
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-01-06 17:47:31 -0500

    Get nutrition data loaded by D3

--- src/index.js
+++ src/index.js
@@ -37,4 +37,32 @@ const handleScrollOnto = (entries, observer) => {
     })
 }
 
+let nutritionData;
+
+d3.csv("nutrition_facts_for_scroller.csv", d => {
+    return {
+      food_name: d["Food name"],
+      serving_size: d["Amount"],
+      fiber: +d["Fiber"],
+      iron: +d["Iron"],
+      magnesium: +d["Magnesium"],
+      potassium: +d["Potassium"],
+      zinc: +d["Zinc"],
+      vitamin_c: +d["Vitamin C"],
+      folate: +d["Folate"],
+      vitamin_b12: +d["Vitamin B-12"],
+      vitamin_a: +d["Vitamin A"],
+      vitamin_d: +d["Vitamin D"],
+      cholesterol: +d["Cholesterol"]
+    };
+}).then(data => {
+    nutritionData = data;
+    // console.log(nutritionData);
+});
+
+const createVisualization = () => {
+    let w = 600;
+    let h = 500;
+
+}
 
