700fc7dcd46c0dc358c6295b0678ff1de3c9af38
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-01-06 16:55:20 -0500

    Add nutrition facts data file

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const createObserver = () => {
     let options = {
       root: null,
       rootMargin: "0px 0px 0px 0px",
-      threshold: .25
+      threshold: .50
     };
 
     observer = new IntersectionObserver(handleScrollOnto, options);
@@ -26,11 +26,11 @@ const createObserver = () => {
 const handleScrollOnto = (entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            // entry.target.style.opacity = "100%";
+            entry.target.style.opacity = "100%";
             entry.target.style.transform = "translateX(0%)";
             // entry.target.style.visibility = "visible";
         } else {
-            // entry.target.style.opacity = "0%";
+            entry.target.style.opacity = "0%";
             // entry.target.style.visibility = "hidden";
             entry.target.style.transform = "translateX(150%)";
         }
