12e364a19cbde48c16261b3c0fba5990e5779a13
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-01-07 18:05:15 -0500

    Make adjustments and do experiments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import './styles/index.scss';
 
-let redBox, blueBox, textBox;
-
 let nutritionData;
 
 d3.csv("nutrition_facts_for_scroller.csv", d => {
@@ -23,21 +21,26 @@ d3.csv("nutrition_facts_for_scroller.csv", d => {
 }).then(data => {
   nutritionData = data;
   console.log(nutritionData);
-  console.log(Object.values(nutritionData[0]).slice(2, -1));
-  createVisualization(nutritionData[0]);
+
+  nutritionData.forEach((foodData, idx) => {
+      createVisualization(foodData, idx);
+  });
+
 });
 
-const createVisualization = objectData => {
+const createVisualization = (foodData, idx) => {
   let w = 600;
   let h = 500;
 
-  let data = Object.values(objectData).slice(2, -1);
+  let data = Object.values(foodData).slice(2, -1);
   let numberOfColumns = 10;
   let maxValue = d3.max(data, function(d) {
     return +d;
   });
   let x_axisLength = w - 50;
   let y_axisLength = h - 50;
+  let targetSlide = "#slide-svg-" + idx;
+  let targetSlideRect = "slide-svg-" + idx + "-rect";
 
   let yScale = d3
     .scaleLinear()
@@ -45,7 +48,8 @@ const createVisualization = objectData => {
     .range([0, y_axisLength]);
 
   let svg = d3
-    .select("#text-box")
+    // .select("#slide-svg-0")
+    .select(`${targetSlide}`)
     .append("svg")
     .attr("width", w)
     .attr("height", h);
@@ -55,6 +59,8 @@ const createVisualization = objectData => {
     .data(data)
     .enter()
     .append("rect")
+    // .attr("class", "#slide-svg-0-rect")
+    .attr("class", `${targetSlideRect}`)
     .attr("x", function(d, i) {
       return i * (x_axisLength / numberOfColumns) + 25;
     })
@@ -65,76 +71,154 @@ const createVisualization = objectData => {
     .attr("height", function(d) {
       return yScale(d);
     })
-    .attr("fill", "red");
+    .attr("fill", "red")
+    .transition()
+    .duration(500);
 };
 
 
 window.addEventListener("load", (e) => {
-    textBox = document.querySelector("#text-box");
-    redBox = document.querySelector("#red-box");
-    blueBox = document.querySelector("#blue-box");
-
-    createObserver();
+    
+    let slides = [];
+    for (let i = 0; i < 19; i++) {
+        let slideName = "#slide-container-" + i;
+        let newSlide = document.querySelector(slideName);
+        slides.push(newSlide);
+    }
+    createObservers(slides);
 }, false);
 
-const createObserver = () => {
-    let observer;
+const createObservers = (slides) => {
     
     let options = {
       root: null,
       rootMargin: "0px 0px 0px 0px",
-      threshold: .75
+      threshold: [.15, .85]
+    //   threshold: .05
     };
 
-    observer = new IntersectionObserver(handleScrollOnto, options);
-    // observer.observe(redBox);
-    observer.observe(textBox);
-    // observer.observe(blueBox);
+    
+    for (let i = 0; i < 18; i++) {
+        handleScrollOntoWrapper(i, options, slides[i]);
+    }
+
 }
 
-const handleScrollOnto = (entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
+const handleScrollOntoWrapper = (idx, options, slide) => {
+    debugger;
+    let foodData = nutritionData[idx];
 
-            let singleFoodData = Object.values(nutritionData[0]).slice(2, -1);
-            let maxValue = d3.max(singleFoodData, function(d) {
-            return +d;
-            });
-            // let x_axisLength = w - 50;
-            let y_axisLength = 450;
+    let targetSlide = ".slide-svg-" + idx;
+    let targetSlideRect = targetSlide + "-rect";
 
-            let yScale = d3
-            .scaleLinear()
-            .domain([0, maxValue])
-            .range([0, y_axisLength]);
+    const handleScrollOnto = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+            // if (entry.intersectionRatio <= 0.80) {
+              let singleFoodData = Object.values(foodData).slice(2, -1);
 
+              // console.log(singleFoodData);
+              console.log(nutritionData[idx].food_name);
+              console.log(entry.intersectionRatio);
+              console.log(entry.boundingClientRect);
+              // createVisualization(foodData, idx);
+              let maxValue = d3.max(singleFoodData, function(d) {
+                return +d;
+              });
+              let y_axisLength = 450;
 
-            d3.selectAll("rect")
-              .data(singleFoodData)
-              .transition()
-              .attr("y", function(d) {
-                return 500 - yScale(d);
-              })
-              .attr("height", function(d) {
-                return yScale(d);
-              })
-              .duration(500)
-            //   .delay(500);
+              let yScale = d3
+                .scaleLinear()
+                .domain([0, maxValue])
+                .range([0, y_axisLength]);
 
-            // createVisualization(nutritionData[0]);
-            // entry.target.style.opacity = "100%";
-            // entry.target.style.transform = "translateX(0%)";
-            // entry.target.style.visibility = "visible";
-        } else {
-            d3.selectAll("rect")
-            .transition()
-            .attr("y", 500)
-            .duration(500);
-            // entry.target.style.opacity = "0%";
-            // entry.target.style.visibility = "hidden";
-            // entry.target.style.transform = "translateX(50%)";
-        }
-    })
+              // d3.selectAll(`${targetSlideRect}`)
+              d3.selectAll(`rect`)
+                .data(singleFoodData)
+                .transition()
+                .attr("y", function(d) {
+                  return 500 - yScale(d);
+                })
+                .attr("height", function(d) {
+                  return yScale(d);
+                })
+                .duration(500);
+            } else {
+            //   console.log(nutritionData[idx].food_name);
+            //   console.log(entry.intersectionRatio);
+              d3.selectAll(`rect`)
+                // d3.selectAll(`${targetSlideRect}`)
+                // d3.selectAll("#slide-svg-0-rect")
+                .transition()
+                .attr("y", 500)
+                .duration(500);
+            }
+        });
+    };
+
+    let observer = new IntersectionObserver(handleScrollOnto, options);
+    observer.observe(slide);
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const handleScrollOnto = (entries, observer) => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+
+//             let singleFoodData = Object.values(nutritionData[0]).slice(2, -1);
+//             let maxValue = d3.max(singleFoodData, function(d) {
+//             return +d;
+//             });
+//             // let x_axisLength = w - 50;
+//             let y_axisLength = 450;
+
+//             let yScale = d3
+//             .scaleLinear()
+//             .domain([0, maxValue])
+//             .range([0, y_axisLength]);
+
+
+//             d3.selectAll("rect")
+//               .data(singleFoodData)
+//               .transition()
+//               .attr("y", function(d) {
+//                 return 500 - yScale(d);
+//               })
+//               .attr("height", function(d) {
+//                 return yScale(d);
+//               })
+//               .duration(500)
+//             //   .delay(500);
+
+//             // createVisualization(nutritionData[0]);
+//             // entry.target.style.opacity = "100%";
+//             // entry.target.style.transform = "translateX(0%)";
+//             // entry.target.style.visibility = "visible";
+//         } else {
+//             d3.selectAll("rect")
+//             .transition()
+//             .attr("y", 500)
+//             .duration(500);
+//             // entry.target.style.opacity = "0%";
+//             // entry.target.style.visibility = "hidden";
+//             // entry.target.style.transform = "translateX(50%)";
+//         }
+//     })
+// }
 
 
