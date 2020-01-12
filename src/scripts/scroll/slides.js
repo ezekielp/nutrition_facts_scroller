a9c3b044c31f8668ee584fbdce53069fc9eb976b
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-01-12 00:09:09 -0500

    Make navbar show label and color when scroll onto slide

--- src/scripts/scroll/slides.js
+++ src/scripts/scroll/slides.js
@@ -9,20 +9,14 @@ export const renderSlide = (options, slide, idx) => {
           rect.classList.add("chart-rect");
         });
 
-        // document
-        //   .querySelector(`#slide-container-${idx}`)
-        //   .classList.remove("opaque");
-        
-        // d3.selectAll(`.slide-svg-${idx}-rect`).on("mouseover", handleMouseover);
+        d3.select(`.slide-svg-${idx}-y-axis`)
+          .transition()
+          .style("opacity", "100%")
+          .duration(500);
 
-        // const handleMouseover = (d, i) => {
-        //   debugger;
-        //   d3.select(this)
-        //     .transition()
-        //     .ease("ease")
-        //     .duration(500)
-        //     .attr("fill", "white");
-        // };
+        let navCircle = document.getElementById(`nav-li-${idx}`);
+        navCircle.classList.add(`nav-li-${idx}`);
+
 
 
         if (document.querySelectorAll(`.slide-svg-${idx - 1}-rect`)) {
@@ -32,49 +26,36 @@ export const renderSlide = (options, slide, idx) => {
                 rect.classList.add("hidden");
                 rect.classList.remove("chart-rect");
               });
+
+            d3.select(`.slide-svg-${idx - 1}-y-axis`)
+              .transition()
+              .style("opacity", "0%")
+              .duration(500);
+            
         }
 
-        // if (document.querySelector(`#slide-container-${idx - 1}`)) {
-        //   document
-        //     .querySelector(`#slide-container-${idx - 1}`)
-        //     .classList.add("opaque");
-        // }
+        if (document.getElementById(`nav-li-${idx - 1}`)) {
+          document.getElementById(`nav-li-${idx - 1}`).classList.remove(`nav-li-${idx - 1}`);
+        }
 
         if (document.querySelectorAll(`.slide-svg-${idx + 1}-rect`)) {
+            // debugger;
             document
               .querySelectorAll(`.slide-svg-${idx + 1}-rect`)
               .forEach(rect => {
                 rect.classList.add("hidden");
                 rect.classList.remove("chart-rect");
               });
-        }
 
-        // if (document.querySelector(`#slide-container-${idx + 1}`)) {
-        //   document
-        //     .querySelector(`#slide-container-${idx + 1}`)
-        //     .classList.add("opaque");            
-        // }
-
-        d3.select(`.slide-svg-${idx}-y-axis`)
-          .transition()
-          .style("opacity", "100%")
-          .duration(500);
-
-
-        if (document.querySelector(`.slide-svg-${idx - 1}-y-axis`)) {
-            d3.select(`.slide-svg-${idx - 1}-y-axis`)
-              .transition()
-              .style("opacity", "0%")
-              .duration(500);
-        }
-
-        if (document.querySelector(`.slide-svg-${idx + 1}-y-axis`)) {
             d3.select(`.slide-svg-${idx + 1}-y-axis`)
               .transition()
               .style("opacity", "0%")
               .duration(500);
+
+            document.getElementById(`nav-li-${idx + 1}`).classList.remove(`nav-li-${idx + 1}`);              
         }
 
+          
       }
     });
   };
