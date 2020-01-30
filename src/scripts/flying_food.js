447244e92afc33ff82011ac47f7b3b71771a012a
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-01-30 10:55:41 -0500

    Make initial fix to stubborn flying food bug

--- src/scripts/flying_food.js
+++ src/scripts/flying_food.js
@@ -5,8 +5,14 @@
         const addFlyingFoodListener = idx => {
             let foodIcon = document.getElementById(`food-svg-container-${idx}`);
 
-            foodIcon.addEventListener("click", e => {
+            let justClicked = false;
+            
+            const flyingFoodClickCallback = e => {
+                if (justClicked) {
+                    return;
+                } else {
                 let foodChildren = foodIcon.childNodes;
+
                 window.setTimeout(() => {
                     if (foodChildren[3]) {
                         for (let i = 0; i < 30; i++) {
@@ -16,43 +22,52 @@
                 }, 3000);
 
                 let movementFunc = newFood => {
-                let start = null;
+                    let start = null;
 
-                let signedOnes = [-1, 1];
-                let randomIdx = Math.floor(Math.random() * 2);
-                let randomlySignedOne = signedOnes[randomIdx];
+                    let signedOnes = [-1, 1];
+                    let randomIdx = Math.floor(Math.random() * 2);
+                    let randomlySignedOne = signedOnes[randomIdx];
 
-                const step = timestamp => {
-                    if (!start) start = timestamp;
-                    let progress = timestamp - start;
-                    newFood.style.transform = "translate(" + (progress * randomlySignedOne) + "px, " + progress + "px)";
-                    if (progress < 2500) {
+                    const step = timestamp => {
+                        if (!start) start = timestamp;
+                        let progress = timestamp - start;
+                        newFood.style.transform = "translate(" + (progress * randomlySignedOne) + "px, " + progress + "px)";
+                        if (progress < 2500) {
+                            window.requestAnimationFrame(step);
+                        }
+                    };
+
                     window.requestAnimationFrame(step);
-                    }
-                };
-
-                window.requestAnimationFrame(step);
                 };
 
                 for (let i = foodCounters[idx]; i < foodCounters[idx] + 30; i++) {
-                let newFood = document.createElement("div");
-                newFood.setAttribute("id", `flying-food-of-type-${idx}-${i}`);
-                newFood.classList.add(`flying-food-${idx}`);
-                newFood.classList.add(`flying-food`);
-                foodIcon.appendChild(newFood);
+                    let newFood = document.createElement("div");
+                    newFood.setAttribute("id", `flying-food-of-type-${idx}-${i}`);
+                    newFood.classList.add(`flying-food-${idx}`);
+                    newFood.classList.add(`flying-food`);
+                    foodIcon.appendChild(newFood);
 
-                let thisOneParticularFood = document.getElementById(
-                    `flying-food-of-type-${idx}-${i}`
-                );
-                thisOneParticularFood.style.top = (Math.random() * -300) + window.scrollY + "px";
-                thisOneParticularFood.style.left =
-                    Math.floor(Math.random() * window.innerWidth) + "px";
+                    let thisOneParticularFood = document.getElementById(
+                        `flying-food-of-type-${idx}-${i}`
+                    );
+                    thisOneParticularFood.style.top = (Math.random() * -300) + window.scrollY + "px";
+                    thisOneParticularFood.style.left =
+                        Math.floor(Math.random() * window.innerWidth) + "px";
 
-                movementFunc(thisOneParticularFood);
+                    movementFunc(thisOneParticularFood);
                 }
 
                 foodCounters[idx] += 10;
-            });
+
+                justClicked = true;
+                window.setTimeout(() => {
+                    justClicked = false;
+                }, 3000);
+                }
+            }
+
+            foodIcon.addEventListener("click", flyingFoodClickCallback);
+
         };
 
         for (let i = 0; i < 18; i++) {
